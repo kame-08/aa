0c79a0461c0356f006bb545532c0dedb30592618
MBP2018 <21cm0108@jec.ac.jp>
2023-02-02 17:31:02 +0900

    Initial Commit

---
+++ aa/aaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aaApp.swift
+//  aa
+//
+//  Created by Ryo on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct aaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
